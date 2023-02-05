45f7dc535775e7b839e9788a189bb27dbea55717
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-02-06 00:52:33 +0600

    add previous code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,34 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    daisyui: {
-      themes: [
-        {
-          default: {
+  theme: {},
+  daisyui: {
+    themes: [
+      {
+        default: {
 
-            "primary": "#fc2420",
+          "primary": "#fc2420",
 
-            "secondary": "#1ae04f",
+          "secondary": "#1ae04f",
 
-            "accent": "#dbef83",
+          "accent": "#dbef83",
 
-            "neutral": "#23252F",
+          "neutral": "#23252F",
 
-            "base-100": "#2F3237",
+          "base-100": "#FFFFFF",
 
-            "info": "#9AB4DF",
+          "info": "#9AB4DF",
 
-            "success": "#20D57A",
+          "success": "#20D57A",
 
-            "warning": "#957C0F",
+          "warning": "#957C0F",
 
-            "error": "#F3381B",
-          },
+          "error": "#F3381B",
         },
-      ],
-    },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 }
+
